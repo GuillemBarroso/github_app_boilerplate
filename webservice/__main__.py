c615c93e5054bc01467fab42907af56868e232c7
guillem.barroso <guillem.barroso@ansys.com>
2022-09-20 14:14:27 +0200

    create and close thank issue after install

--- webservice/__main__.py
+++ webservice/__main__.py
@@ -48,26 +48,54 @@ async def webhook(request):
 
 @router.register("installation", action="created")
 async def repo_installation_added(event, gh, *args, **kwargs):
+    # Inputs:
+    #   - event: representation of GitHub's webhook event.
+    #   - gh: gidgethub GitHub API to make API calls to GitHub.
+    
+    # Get installation access token 
     installation_id = event.data["installation"]["id"]
-
     installation_access_token = await apps.get_installation_access_token(
         gh,
         installation_id=installation_id,
         app_id=os.environ.get("GH_APP_ID"),
         private_key=os.environ.get("GH_PRIVATE_KEY")
     )
-    repo_name = event.data["repositories"][0]["full_name"]
-    url = f"/repos/{repo_name}/issues"
-    response = await gh.post(
-        url,
-                     data={
-        'title': 'Thanks for installing my bot',
-        'body': 'Thanks!',
-            },
-        oauth_token=installation_access_token["token"]
-                             )
-    print(response)
 
+    # Create issue thanking maintainer for installing bot
+    maintainer = event.data["sender"]["login"]
+    message = f"Thanks for installing me, @{maintainer}! (I'm a bot)."
+
+    for repository in event.data["repositories_added"]:
+        url = f"/repos/{repository['full_name']}/issues"
+        response = await gh.post(
+            url,
+            data={
+                "title": "Mariatta's bot was installed",
+                "body": message
+            },
+            oauth_token=installation_access_token["token"],
+        )
+    
+    # Close issue
+    issue_url = response["url"]
+    await gh.patch(issue_url, data={"state": "closed"},
+        oauth_token=installation_access_token["token"]
+    )
+
+    # Different code from what appers in Mariatta's blog
+
+    # repo_name = event.data["repositories"][0]["full_name"]
+    # url = f"/repos/{repo_name}/issues"
+    # response = await gh.post(
+    #     url,
+    #                  data={
+    #     'title': 'Thanks for installing my bot',
+    #     'body': 'Thanks!',
+    #         },
+    #     oauth_token=installation_access_token["token"]
+    #     )
+
+    # print(response)
 
 
 if __name__ == "__main__":  # pragma: no cover
